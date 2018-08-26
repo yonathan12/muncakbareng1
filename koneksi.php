beb2dfc09468eb8b17d51702889e9e863b5b6d1c
yonathan <jrizky.jonathan@gmail.com>
2018-08-26 20:38:30 +0700

    Ubah koneksi.php dari mysql ke mysqli

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,8 @@
 <?php
-
 $koneksi = mysqli_connect("localhost","root","","muncakbareng");
 //cek koneksi
 if (mysqli_connect_errno()) {
 	# code...
 	echo "Koneksi Gagal".mysqli_connect_error();
 }
-
 ?>
