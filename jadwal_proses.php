a33a1b2c174d6056aa194f8cbb53e19bb35a2fd2
yonathan <jrizky.jonathan@gmail.com>
2018-08-26 20:49:03 +0700

    Migrasi jadwal_proses.php dari mysql ke mysqli

--- jadwal_proses.php
+++ jadwal_proses.php
@@ -18,9 +18,12 @@ $jam_mp = $_POST['jam_mp'];
 $cp = $_SESSION['nama'];
 $telpon = $_POST['telpon'];
 $keterangan = $_POST['keterangan'];
+$tanggal = date("Y-m-d H:i:s");
 $ip = $_SERVER['REMOTE_ADDR'];
 
-$simpan = mysql_query("INSERT INTO jadwal (gn_tujuan,naik_via,turun_via,tanggal_naik,tanggal_turun,mp,tanggal_mp,jam_mp,cp,telpon,keterangan,ip,dibuat) VALUES ('$gn_tujuan','$naik_via','$turun_via','$tanggal_naik','$tanggal_turun','$mp','$tanggal_mp','$jam_mp','$cp','$telpon','$keterangan','$ip',NOW())") or die(mysql_error());
+// $simpan = mysql_query("INSERT INTO jadwal (gn_tujuan,naik_via,turun_via,tanggal_naik,tanggal_turun,mp,tanggal_mp,jam_mp,cp,telpon,keterangan,ip,dibuat) VALUES ('$gn_tujuan','$naik_via','$turun_via','$tanggal_naik','$tanggal_turun','$mp','$tanggal_mp','$jam_mp','$cp','$telpon','$keterangan','$ip','$tanggal')") or die(mysql_error());
+
+$simpan = mysqli_query($koneksi, "INSERT INTO jadwal VALUES ('','$gn_tujuan','$naik_via','$turun_via','$tanggal_naik','$tanggal_turun','$mp','$tanggal_mp','$jam_mp','$cp','$telpon','$keterangan','$ip','$tanggal')");
 
 if($simpan){
 	echo '<script language="javascript">
