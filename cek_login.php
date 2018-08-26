d2a3ef838df3e4d1a3a47d4034d85d798201dfb2
yonathan <jrizky.jonathan@gmail.com>
2018-08-26 20:58:23 +0700

    Migrasi cek login dari mysql ke mysqli

--- cek_login.php
+++ cek_login.php
@@ -8,9 +8,10 @@ if(!empty($_POST)){
     $email = $_POST['email'];
     $password = md5($_POST['password']);
 
-    $sql = mysql_query("SELECT * FROM user WHERE email='$email' and password='$password'");
-        if(mysql_num_rows($sql)){
-            $cek = mysql_fetch_array($sql);
+    $sql = mysqli_query($koneksi, "SELECT * FROM user WHERE email='$email' and password='$password'");
+
+    $cek = mysqli_num_rows($sql);
+        if($cek > 0){
             $_SESSION ['isLoggedIn']=1;
             $_SESSION['nama'] = $cek['nama'];
             $_SESSION['Id'] = $cek['Id'];
@@ -22,16 +23,14 @@ if(!empty($_POST)){
             document.location='index';
         </script>
         <?php
-    }
+        }
 
-            $log = mysql_query("INSERT INTO user_log_ppdb (email,tanggal_akses,ip) VALUES ('$email',NOW(),'$ip')")  or die (mysql_error());
-            //$logupdate = mysql_query("UPDATE user_log SET lastlogin='NOW()' WHERE username=$username") or die (mysql_error());
-             
+        $log = mysqli_query($koneksi,"INSERT INTO user_log_ppdb (email,tanggal_akses,ip) VALUES ('$email',NOW(),'$ip')");
         }else{
-           echo '<script language="javascript">
-             alert("Username Atau Password Salah");
-             location.href="index";
-                </script>';
-                exit();
-              }
+            echo '<script language="javascript">
+            alert("Username Atau Password Salah");
+            location.href="index";
+            </script>';
+            exit();
+        }
 ?>
