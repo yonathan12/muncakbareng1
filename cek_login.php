1faa0915b24c562db2508d6287c4f0fe11337925
yonathan <jrizky.jonathan@gmail.com>
2018-08-28 21:40:19 +0700

    Perbaikan session mysqli

--- cek_login.php
+++ cek_login.php
@@ -3,34 +3,24 @@ session_start();
 include 'koneksi.php';
 $ip       = $_SERVER['REMOTE_ADDR'];
  
-if(!empty($_POST)){
      
     $email = $_POST['email'];
     $password = md5($_POST['password']);
 
     $sql = mysqli_query($koneksi, "SELECT * FROM user WHERE email='$email' and password='$password'");
 
-    $cek = mysqli_num_rows($sql);
-        if($cek > 0){
+    if (mysqli_num_rows($sql) > 0 ) {
+        $cek = mysqli_fetch_array($sql);
             $_SESSION ['isLoggedIn']=1;
             $_SESSION['nama'] = $cek['nama'];
             $_SESSION['Id'] = $cek['Id'];
             header("location:home");
-        }else{
+    } else {
         ?>
         <script language="JavaScript">
             alert('Username & Password Salah');
             document.location='index';
         </script>
         <?php
-        }
-
-        $log = mysqli_query($koneksi,"INSERT INTO user_log_ppdb (email,tanggal_akses,ip) VALUES ('$email',NOW(),'$ip')");
-        }else{
-            echo '<script language="javascript">
-            alert("Username Atau Password Salah");
-            location.href="index";
-            </script>';
-            exit();
         }
 ?>
